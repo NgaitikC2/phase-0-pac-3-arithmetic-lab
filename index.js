00764db1deda3d7a78415607ee17d8b303e2db65
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-08-19 15:40:08 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+var a = 1500;
+var b = 6;
+
+function add(a, b) {
+    return (a+b);
+}
+
+var a = 1000;
+var b = 358;
+
+function subtract(a, b) {
+    return (a-b);
+}
+
+var a = 596.4;
+var b = 9;
+
+function multiply(a, b) {
+    return (a*b);
+}
+
+var a = 1516.2790697674419
+var b = 100
+
+function divide(a, b) {
+    return (a/b);
+}
+
+function increment(n) {
+    return n+1;
+}
+
+function decrement(n) {
+    return n-1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
